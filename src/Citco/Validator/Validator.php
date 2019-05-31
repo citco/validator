2e6a7361927724df98e279786b883193697f6bb6
Misagh Laghaee <misagh1663@yahoo.com>
2019-05-31 19:44:35 +0430

    Change the method to be public

--- src/Citco/Validator/Validator.php
+++ src/Citco/Validator/Validator.php
@@ -3,15 +3,8 @@ namespace Citco\Validator;
 
 class Validator extends \Illuminate\Validation\Validator {
 
-	protected function validateEmail($attribute, $value)
+	public function validateEmail($attribute, $value)
 	{
-		if((! str_contains($value, '+')) && filter_var($value, FILTER_VALIDATE_EMAIL))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return ! str_contains($value, '+') && filter_var($value, FILTER_VALIDATE_EMAIL);
 	}
 }
